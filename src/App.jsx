ba33f6bf40046b1210d7a0b14cdce25786f9c009
Guilherme Lins <guilhermelins978@gmail.com>
2024-01-04 13:17:22 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+import { UserStorage } from "./UserContext";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react'
+import { Route, Routes, Navigate } from 'react-router-dom'
+import LoginForm from './LoginForm'
+import LoginCreate from './LoginCreate'
+import LoginPasswordReset from './LoginPasswordReset'
+import LoginPasswordLost from './LoginPasswordLost'
+import { UserContext } from '../../UserContext'
+
+const Login = () => {
+  const { login } = useContext(UserContext)
+
+  if(login === true) return <Navigate to='/conta'/>
+  return (
+    <div>
+     <Routes>
+        <Route path='/' element={<LoginForm/>}/> 
+        <Route path='criar' element={<LoginCreate/>}/> 
+        <Route path='perdeu' element={<LoginPasswordLost/>}/> 
+        <Route path='resetar' element={<LoginPasswordReset/>}/> 
+     </Routes>
+    </div>
+  )
+}
+
+export default Login
