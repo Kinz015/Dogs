2122ff9770f1fcb2d2f383abcc594cf3d1a3d374
Guilherme Lins <guilhermelins978@gmail.com>
2024-01-04 16:39:52 -0300

    deu certo

--- src/Components/Login/LoginCreate.jsx
+++ src/Components/Login/LoginCreate.jsx
@@ -1,25 +1,45 @@
-import React from "react";
+import React, { useContext } from "react";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
+import Error from "../Helper/Error";
 import useForm from "../../Hooks/useForm";
+import { USER_POST } from "../../api";
+import { UserContext } from "../../UserContext";
+import useFetch from "../../Hooks/useFetch";
 
 const LoginCreate = () => {
-
   const username = useForm();
-  const email = useForm('email');
-  const password = useForm('password');
+  const email = useForm("email");
+  const password = useForm();
 
-  function handleSubmit(ev) {
-    ev.preventDefault9;
+  const { userLogin } = useContext(UserContext);
+  const {loading, error, request} = useFetch();
+
+  async function handleSubmit(ev) {
+    ev.preventDefault();
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+    const {response} = await request(url, options);
+    if (response.ok) userLogin(username.value, password.value);
+    console.log(response);
   }
   return (
     <section className="animeLeft">
       <h1 className="title">Cadastre-se</h1>
       <form onSubmit={handleSubmit}>
-        <Input label="Usuário" type="type" name="username" {...username}></Input>
-        <Input label="Email" type="type" name="email" {...email}></Input>
-        <Input label="Senha" type="type" name="senha" {...password}></Input>
-        <Button>Cadastrar</Button>
+        <Input
+          label="Usuário"
+          type="text"
+          name="username"
+          {...username}
+        ></Input>
+        <Input label="Email" type="email" name="email" {...email}></Input>
+        <Input label="Senha" type="password" name="senha" {...password}></Input>
+        {loading ? <Button disabled>Cadastrando...</Button> : <Button>Cadastrar</Button>}
+        <Error error={error}/>
       </form>
     </section>
   );
