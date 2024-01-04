6144ef0d4e526a54d6c4cbe7cbefc201b51c7273
Guilherme Lins <guilhermelins978@gmail.com>
2024-01-04 15:43:22 -0300

    atualização

--- src/Components/Login/LoginCreate.jsx
+++ src/Components/Login/LoginCreate.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
 
 const LoginCreate = () => {
-  return (
-    <div>LoginCreate</div>
-  )
-}
 
-export default LoginCreate
+  const username = useForm();
+  const email = useForm('email');
+  const password = useForm('password');
+
+  function handleSubmit(ev) {
+    ev.preventDefault9;
+  }
+  return (
+    <section className="animeLeft">
+      <h1 className="title">Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="type" name="username" {...username}></Input>
+        <Input label="Email" type="type" name="email" {...email}></Input>
+        <Input label="Senha" type="type" name="senha" {...password}></Input>
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
+  );
+};
+
+export default LoginCreate;
