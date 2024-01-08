a1b89445400948baafcad6142d90c25b0534012d
Guilherme Lins <guilhermelins978@gmail.com>
2024-01-08 18:09:36 -0300

    s
    s

--- src/Components/User/UserPhotoPost.jsx
+++ src/Components/User/UserPhotoPost.jsx
@@ -1,9 +1,26 @@
-
+import Input from '../Forms/Input';
+import styles from './UserPhotoPost.module.css'
 import React from 'react'
 
 const UserPhotoPost = () => {
+  function handleSubmit(ev) {
+    ev.preventDefault();
+  }
+
+  function handleImgChange() {
+
+  }
+
   return (
-    <div>Postar fotos</div>
+    <section className={`${styles.photoPost} animeLeft`}>
+      <form onSubmit={handleSubmit}>
+        <Input label="Nome" type="text" name="nome"/>
+        <Input label="Peso" type="text" name="peso"/>
+        <Input label="Idade" type="text" name="idade"/>
+        <input type="file" name="img" id='img' onChange={handleImgChange}/>
+        <Button>Enviar</Button>
+      </form>
+    </section>
   )
 }
 
