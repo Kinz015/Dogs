753afc2ab280e682f4c00cebf7eca0467ebcb4bb
Guilherme Lins <guilhermelins978@gmail.com>
2024-01-12 14:39:40 -0300

    ATÉ AQUI TA BACANA

--- src/Components/User/UserStatus.jsx
+++ src/Components/User/UserStatus.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Head from "../Helper/Head";
+import useFetch from "../../Hooks/useFetch";
 
 const UserStatus = () => {
+  const {data, error, loading, request} = useFetch()
+  useEffect(() => {
+    async function getData() {
+      const {url, response} = GET_ST
+    }
+  }, [request])
+
   return (
     <div>
       <Head title="Estatísticas" />
--- src/api.jsx
+++ src/api.jsx
@@ -140,3 +140,15 @@ export function PASSWORD_RESET(body) {
     },
   };
 }
+
+export function STATS_GET(body) {
+  return {
+    url: API_URL + "/api/stats",
+    options: {
+      method: "GET",
+      Headers: {
+        Authorization: "Bearer " + window.localStorage.getItem("token"),
+      },
+    },
+  };
+}
